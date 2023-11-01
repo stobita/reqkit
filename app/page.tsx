837aeb2510b4d196fa8fc24cdcf1e5157546d501
飛田惣吉 <stobita@hidasoukichinoMacBook-Pro.local>
2023-11-01 23:00:24 +0900

    feat: update url request

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,43 @@
-import Image from "next/image";
+"use client";
+import { invoke } from "@tauri-apps/api/tauri";
+import React from "react";
 
 export default function Home() {
+  const [url, setUrl] = React.useState<string>("");
+  const [res, setRes] = React.useState<string>("");
+  const [error, setError] = React.useState<string>("");
+  const request = async () => {
+    invoke<string>("request", { url })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+      });
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    request();
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError("");
+    setUrl(e.target.value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form className="flex flex-col items-center justify-center">
+    <main className="h-screen grid place-items-center">
+      <form onSubmit={onSubmit} className="">
         <input
-          className="border-2 border-gray-300 rounded-md p-2 mb-4"
+          className="border-2 border-gray-600 rounded-md p-2 bg-black"
           type="text"
-          placeholder="Name"
+          placeholder="url"
+          value={url}
+          onChange={onChange}
         />
       </form>
+      <p className="text-white">{res}</p>
+      <p className="text-red-500">{error}</p>
     </main>
   );
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,34 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::collections::HashMap;
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![request])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn request(url: String) -> String {
+    println!("Requesting: {}", url);
+    let response = reqwest::blocking::get(&url);
+    match response {
+        Err(e) => {
+            println!("Error: {:?}", e);
+            return format!("Error: {:?}", e);
+        }
+        Ok(res) => {
+            let parsed_response = res.json::<HashMap<String, String>>();
+            match parsed_response {
+                Err(e) => return format!("Parsing error: {:?}", e),
+                Ok(hash_map) => {
+                    let resp_str = format!("{:#?}", hash_map);
+                    println!("{}", resp_str);
+                    return resp_str;
+                }
+            }
+        }
+    }
 }
