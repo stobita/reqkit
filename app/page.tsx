53e47f67bb9470c646db65d282e1dfefcb88f987
飛田惣吉 <stobita@hidasoukichinoMacBook-Pro.local>
2023-11-06 23:33:44 +0900

    feat: improve window size

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,34 @@
 "use client";
+
 import { invoke } from "@tauri-apps/api/tauri";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function Home() {
   const [url, setUrl] = React.useState<string>("");
   const [res, setRes] = React.useState<string>("");
   const [error, setError] = React.useState<string>("");
+  const [height, setHeight] = React.useState<number>(0);
+  const ref = useRef<HTMLDivElement>(null);
   const request = async () => {
-    invoke<string>("request", { url })
-      .then((res) => {
-        console.log(res);
-        setRes(res);
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(err);
-      });
+    try {
+      const res = await invoke<string>("request", { url });
+      setRes(res);
+    } catch (err) {
+      console.log(err);
+      // setError(err);
+    }
   };
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    import("@tauri-apps/api/window").then(
+      async ({ appWindow, LogicalSize }) => {
+        if (!ref.current) return;
+        const height = ref.current.offsetHeight + 100;
+        console.log("height", height);
+        await appWindow.setSize(new LogicalSize(300, height));
+      },
+    );
+  });
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     request();
@@ -27,7 +39,11 @@ export default function Home() {
   };
 
   return (
-    <main className="h-screen grid place-items-center">
+    <main
+      ref={ref}
+      className="h-screen grid place-items-center max-w-xs auto-cols-auto auto-rows-auto"
+      style={{ height: `${height}` }}
+    >
       <form onSubmit={onSubmit} className="">
         <input
           className="border-2 border-gray-600 rounded-md p-2 bg-black"
@@ -37,7 +53,7 @@ export default function Home() {
           onChange={onChange}
         />
       </form>
-      <p className="text-white">{res}</p>
+      <p className="text-white max-w-xs">{res}</p>
       <p className="text-red-500">{error}</p>
     </main>
   );
