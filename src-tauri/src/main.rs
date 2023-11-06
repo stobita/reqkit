93e5f3060522da1060bd49d6320d5bf1554f8578
飛田惣吉 <stobita@hidasoukichinoMacBook-Pro.local>
2023-11-06 22:05:06 +0900

    fix: server code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::collections::HashMap;
-
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![request])
@@ -13,7 +11,15 @@ fn main() {
 #[tauri::command]
 fn request(url: String) -> String {
     println!("Requesting: {}", url);
-    let response = reqwest::blocking::get(&url).unwrap();
-    println!("status: {}", response.status());
-    return response.text().unwrap();
+    let response = reqwest::blocking::get(&url);
+    if response.is_err() {
+        return String::from("Error");
+    }
+    let resValue = match response {
+        Ok(res) => res,
+        Err(_) => return String::from("Error"),
+    };
+
+    println!("status: {}", resValue.status());
+    return resValue.text().unwrap();
 }
